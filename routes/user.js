0b7ca4f18e4c13082335708005a3855c8079ad7c
Vasanthivuppala <vasanthivuppala890@gmail.com>
2026-04-03 16:12:12 +0530

    updated project files

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const wrapasync=require("../utils/wrapasync.js");
+const passport = require("passport");
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
@@ -20,4 +21,16 @@ router.post("/signup",wrapasync(async(req,res) =>{
     res.redirect("/signup");
   }
 }));
+
+router.get("/login" ,(req,res) =>{
+  req.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",
+  {failureRedirect:"/login",
+  failureFlash:true}),
+async(req,res)=>{
+req.flash("success","Welcome back to wanderlust!");
+res.redirect("/listings");
+});
 module.exports=router;
